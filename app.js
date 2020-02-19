322e36aef58d30e730cf8490eb78ec95f3ea37c7
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-02-19 16:06:30 -0300

    responsive menu mobile added

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-$('.burger-menu-icon').on('click', function () {
-    $(this).toggleClass("burger-menu-icon-opened");
-    $(this).toggleClass("burger-menu-icon-closed");
+
+var t1 = new TimelineMax({ paused: true });
+
+t1.to(".one", 0.8, {
+    y: 6,
+    rotation: 45,
+    ease: Expo.easeInOut
+});
+t1.to(".two", 0.8, {
+    y: -6,
+    rotation: -45,
+    ease: Expo.easeInOut,
+    delay: -1
 });
 
-$('.burger-menu-icon').on('click', function () {
-    $(".burger-menu").toggleClass("burger-active");
+t1.to(".menu", 2, {
+    top: "0%",
+    ease: Expo.easeInOut,
+    delay: -1.7
 });
+
+t1.staggerFrom(".menu ul li", 1, { x: -200, opacity: 0, ease: Expo.easeOut }, 0.1);
+
+t1.reverse();
+$(document).on("click", ".toggle-btn", function () {
+    t1.reversed(!t1.reversed());
+});
+$(document).on("click", "a", function () {
+    t1.reversed(!t1.reversed());
+});
+
+
 
